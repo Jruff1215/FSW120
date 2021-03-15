3387b45d0c354a109018d66f5b3573f3934a8625
Jruff1215 <Jamie.Ruffin@bryanuniversity.edu>
2021-03-15 01:47:21 -0400

    TodoApp

---
+++ assignments/week1/ToDoApp/my-app/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './App.css';
+
+function App() {
+  return(
+    <div>
+      <input type="checkbox" />
+      <p>Wake Up</p>
+
+      <input type="checkbox" />
+      <p>Shower</p>
+
+      <input type="checkbox" />
+      <p>Get Dressed</p>
+
+      <input type="checkbox" />
+      <p>Eat</p>
+    </div>
+  )
+
+}
+
+export default App;
